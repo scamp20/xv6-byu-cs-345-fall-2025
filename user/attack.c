496b154d554bfa80b16958d324ed1ed3002cc5ff
scamp20 <seth.campbell117@gmail.com>
2025-09-22 17:53:32 -0600

    lab2 done

--- user/attack.c
+++ user/attack.c
@@ -8,6 +8,14 @@ main(int argc, char *argv[])
 {
   // your code here.  you should write the secret to fd 2 using write
   // (e.g., write(2, secret, 8)
+  char *end = sbrk(PGSIZE*32);
+  char *str = "very very secret pw is:   ";
+  for (int i = 0; i < 32; i++) {
+    if (memcmp(end + (i * PGSIZE) + 8, str, 24) == 0) {
+      write(2, end + (i * PGSIZE) + 8 + 24, 8);
+      exit(0);
+    }
+  }
 
   exit(1);
 }
