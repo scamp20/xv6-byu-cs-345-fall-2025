8e7c828c8578aa854ccc03e540f621feba30dfc5
Ubuntu <ubuntu@ip-10-0-22-124.us-west-2.compute.internal>
2025-10-31 04:03:00 +0000

    unbind

--- user/nettest.c
+++ user/nettest.c
@@ -108,6 +108,7 @@ rx(char *name)
     ok += 1;
   }
 
+	unbind(2000);
   printf("%s: OK\n", name);
 
   return 1;
@@ -216,6 +217,8 @@ rx2()
       return 0;
     }
   }
+  unbind(2000);
+  unbind(2001);
 
   printf("rx2: OK\n");
 
@@ -297,6 +300,7 @@ ping0()
     return 0;
   }
 
+  unbind(2004);
   printf("ping0: OK\n");
 
   return 1;
@@ -357,6 +361,7 @@ ping1()
     }
   }
 
+  unbind(2005);
   printf("ping1: OK\n");
 
   return 1;
@@ -434,6 +439,8 @@ ping2()
     }
   }
 
+  unbind(2006);
+  unbind(2007);
   printf("ping2: OK\n");
 
   return 1;
@@ -594,6 +601,8 @@ ping3()
     return 0;
   }
 
+  unbind(2008);
+  unbind(2009);
   printf("ping3: OK\n");
 
   return 1;
@@ -807,6 +816,7 @@ dns()
   }
 
   if(dns_rep(ibuf, cc)){
+    unbind(10000);
     printf("dns: OK\n");
     return 1;
   } else {
